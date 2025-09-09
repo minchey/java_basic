f62dd6c1951dc0c7f05f5ad4fb7ca3dc2ef936c6
minchey <nolam@naver.com>
2025-09-09 21:05:11 +0900

    스캐너 사용

---
+++ src/class1/JavaScanner.java
@@ -0,0 +1,17 @@
+package class1;
+import java.util.Scanner;
+
+public class JavaScanner {
+
+    public static void main(String[] args) {
+        System.out.println("나이가 어떻게 되시나요?");
+
+        Scanner scanner = new Scanner(System.in);
+        int age = scanner.nextInt();
+
+        System.out.println(age + "살 입니다.");
+        System.out.println("이름이 어떻게 되세요?");
+        String name = scanner.next();
+        System.out.println(name + "입니다.");
+    }
+}
